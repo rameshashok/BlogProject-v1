80444061bf1cb35b88c8237dbe8c52d6be2f4135
rameshashok <contactrameshashok@gmail.com>
2020-11-03 12:16:32 +0530

    added the build files

---
+++ build/precache-manifest.2620c44dc0e3a5a7d1a1bdd3450116bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1efec8f7e57fd8099c7e12e67251f66e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f75d4366d494a5c1b412",
+    "url": "/static/css/main.8d353cf8.chunk.css"
+  },
+  {
+    "revision": "5f756b03359699ee9969",
+    "url": "/static/js/2.3f19b41b.chunk.js"
+  },
+  {
+    "revision": "8d190e2d02f8e768921a63f56a741895",
+    "url": "/static/js/2.3f19b41b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f75d4366d494a5c1b412",
+    "url": "/static/js/main.b9037477.chunk.js"
+  },
+  {
+    "revision": "585b3be644210b7b4592",
+    "url": "/static/js/runtime-main.fa5471f1.js"
+  }
+]);
