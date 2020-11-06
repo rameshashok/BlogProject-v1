71aa1217217bdbac2b333dd330207c3636443333
RN085052 <Ramesh.Natarajan@cerner.com>
2020-11-06 15:11:36 +0530

    Update AddPost.js

--- src/AddPost/AddPost.js
+++ src/AddPost/AddPost.js
@@ -2,32 +2,39 @@ import React from "react";
 import {Redirect} from "react-router";
 import { Editor } from 'react-draft-wysiwyg';
 import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
-import { EditorState } from 'draft-js';
+import {convertFromRaw, EditorState} from 'draft-js';
+
+const content = {"entityMap":{},"blocks":[{"key":"637gr","text":"Initialized from content state.","type":"unstyled","depth":0,"inlineStyleRanges":[],"entityRanges":[],"data":{}}]};
 
 class AddPost extends React.Component {
+    postContent = convertFromRaw(content);
+
     state = {
         title: '',
         // postContent: '',
         toHome: false,
-        postContent: EditorState.createEmpty()
+        postContent: {}
+        // postContent: EditorState.createEmpty(),
+        // postContent: EditorState.createWithContent(convertFromRaw(initialState))
     };
 
     handleSubmit = (event) => {
         event.preventDefault();
         this.props.onSubmit({
             title: this.state.title,
-            // postContent: this.state.postContent
+            // postContent: convertFromRaw(this.state.postContent)
         });
         this.setState(() => ({toHome: true}));
     };
 
     onEditorStateChange = (postContent) => {
-        this.setState({postContent})
+        this.setState({postContent});
+        console.log(postContent)
     }
 
     imageUpload = (src) => {
         this.setState(() => (
-            {postContent: {data: { link: src}}}
+            this.state.postContent.data.link = src
         ))
     };
 
@@ -54,6 +61,7 @@ class AddPost extends React.Component {
                     // value={this.state.postContent}
                     // onChange={event => this.setState({postContent: event.target.value})}
                     postContent={postContent}
+                    initialContentState={postContent}
                     wrapperClassName="demo-wrapper"
                     editorClassName="demo-editor"
                     toolbarClassName="toolbar-class"
