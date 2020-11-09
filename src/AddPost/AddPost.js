f2729292728d8f40abd8975804ff5fc05081e334
RN085052 <Ramesh.Natarajan@cerner.com>
2020-11-09 18:01:32 +0530

    outputing the content as html

--- src/AddPost/AddPost.js
+++ src/AddPost/AddPost.js
@@ -2,42 +2,28 @@ import React from "react";
 import {Redirect} from "react-router";
 import { Editor } from 'react-draft-wysiwyg';
 import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
-import {convertFromRaw, EditorState} from 'draft-js';
-
-const content = {"entityMap":{},"blocks":[{"key":"637gr","text":"Initialized from content state.","type":"unstyled","depth":0,"inlineStyleRanges":[],"entityRanges":[],"data":{}}]};
+import { EditorState } from 'draft-js';
 
 class AddPost extends React.Component {
-    postContent = convertFromRaw(content);
-
     state = {
         title: '',
-        // postContent: '',
         toHome: false,
-        // postContent: {}
         postContent: EditorState.createEmpty(),
-        // postContent: EditorState.createWithContent(convertFromRaw(initialState))
     };
 
     handleSubmit = (event) => {
         event.preventDefault();
         this.props.onSubmit({
             title: this.state.title,
-            // postContent: convertFromRaw(this.state.postContent)
+            postContent: this.state.postContent
         });
         this.setState(() => ({toHome: true}));
     };
 
     onEditorStateChange = (postContent) => {
         this.setState({postContent});
-        console.log(postContent)
+        console.log({postContent})
     }
-
-    imageUpload = (src) => {
-        this.setState(() => (
-            // this.state.postContent.data.link = src
-            {postContent: {data: {link: src}}}
-        ))
-    };
 
     render() {
         const { postContent } = this.state;
@@ -59,19 +45,9 @@ class AddPost extends React.Component {
                 <Editor
                     placeholder="Content"
                     className="form-control blogContent"
-                    // value={this.state.postContent}
-                    // onChange={event => this.setState({postContent: event.target.value})}
                     postContent={postContent}
-                    // initialContentState={postContent}
                     wrapperClassName="demo-wrapper"
                     editorClassName="demo-editor"
-                    toolbarClassName="toolbar-class"
-                    toolbar={{
-                        image: {
-                            uploadEnabled: true,
-                            uploadCallback: this.imageUpload
-                        }
-                    }}
                     onEditorStateChange={this.onEditorStateChange}
                     required />
                     <br/>
--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { convertToRaw } from 'draft-js';
+import draftToHtml from 'draftjs-to-html';
 
 class Card extends React.Component {
     render() {
@@ -6,7 +8,13 @@ class Card extends React.Component {
             <div className="card list-group-item">
                 <div className="Card card-body">
                     <div className="title card-title">{this.props.title}</div>
-                    <div className="article card-text">{this.props.postContent}</div>
+                    <div className="article card-text">
+                        {
+                            draftToHtml(
+                                convertToRaw(this.props.postContent.getCurrentContent())
+                            )
+                        }
+                    </div>
                 </div>
             </div>
         );
