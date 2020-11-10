9b7878efb2169fd4f3f87ac97a3ef6e1f82f138b
RN085052 <Ramesh.Natarajan@cerner.com>
2020-11-10 16:32:40 +0530

    integrated the addpost editor functionality completely

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,19 +1,40 @@
 import React from "react";
 import { convertToRaw } from 'draft-js';
 import draftToHtml from 'draftjs-to-html';
+import { EditorState, ContentState } from 'draft-js';
+import htmlToDraft from 'html-to-draftjs';
+import { Editor } from 'react-draft-wysiwyg';
 
 class Card extends React.Component {
+    constructor(props) {
+        super(props);
+        const html = draftToHtml(
+            convertToRaw(this.props.postContent.getCurrentContent())
+        );
+        const contentBlock = htmlToDraft(html);
+        if (contentBlock) {
+            const contentState = ContentState.createFromBlockArray(contentBlock.contentBlocks);
+            const editorState = EditorState.createWithContent(contentState);
+            this.state = {
+                editorState,
+            };
+        }
+    }
+
     render() {
+        const { editorState } = this.state;
         return (
             <div className="card list-group-item">
                 <div className="Card card-body">
                     <div className="title card-title">{this.props.title}</div>
                     <div className="article card-text">
-                        {
-                            draftToHtml(
-                                convertToRaw(this.props.postContent.getCurrentContent())
-                            )
-                        }
+                        <Editor
+                            toolbarClassName="content-display-toolbar"
+                            defaultEditorState={editorState}
+                            contentEditable={false}
+                            readOnly
+                            toolbarOnFocus
+                        />
                     </div>
                 </div>
             </div>
